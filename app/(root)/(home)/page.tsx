58770c94921b45424c9bca04e7b4481892bc7427
TarakaKoda <taraka.koda@gmail.com>
2024-02-20 21:55:28 +0530

    Build leftsidebar component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,6 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="h-screen">
-     
     </div>
   )
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,19 @@
 import NavBar from "@/components/shared/navbar/NavBar";
-import React, { ReactNode } from "react";
+import LeftSideBar from "@/components/shared/sidebar/LeftSideBar";
+import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="background-light850_dark100 relative">
-      <NavBar/>
+      <NavBar />
       <div className="flex">
-        LeftSideBar
+        <LeftSideBar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
         RightSideBar
       </div>
-      Toaster
+      {/* Toaster */}
     </main>
   );
 };
