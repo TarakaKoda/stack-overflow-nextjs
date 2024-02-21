131bc57ad1824bd150e13b36d44a5323b0c528a2
TarakaKoda <taraka.koda@gmail.com>
2024-02-21 15:45:24 +0530

    Build a reusable NoResult component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,44 @@
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
+const questions = [
+  {
+    _id: 1,
+    title: "Cascading delete in SQLAlchemy?",
+    tags: [
+      { _id: 1, name: "python " },
+      { _id: 2, name: "sql " },
+    ],
+    author: "John Doe",
+    upvotes: 12,
+    views: 2,
+    answers: 5,
+    createdAt: "2021-09-01T12:00:00.000z",
+  },
+  {
+    _id: 2,
+    title: "How to center a dev?",
+    tags: [
+      { _id: 3, name: "css" },
+      { _id: 4, name: "html" },
+    ],
+    author: "John Doe",
+    upvotes: 1,
+    views: 3,
+    answers: 1,
+    createdAt: "2021-09-01T12:00:00.000z",
+  },
+];
+
 export default function Home() {
   return (
     <>
-      <div className="flex w-full flex-col-reverse justify-between sm:items-center gap-4 sm:flex-row ">
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
         <Link href={`/ask-question`} className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
@@ -16,19 +46,35 @@ export default function Home() {
           </Button>
         </Link>
       </div>
-      <div className="flex mt-11 justify-between gap-5 max-sm:flex-col sm:items-center">
-    <LocalSearchBar
-    route="/"
-    iconPosition="left"
-    imgSrc="/assets/icons/search.svg"
-    placeholder="Search for questions"
-    otherClasses="flex-1"/>
-    <Filter filters={HomePageFilters}
-      otherClasses="min-h-[56px] sm:min-w-[170px]"
-      containerClasses="hidden max-md:flex"
-    />
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
-    <HomeFilters/>
+      <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6 ">
+        {questions.length > 0 ? (
+          questions.map((question) => "QuestionCard")
+        ) : (
+          <NoResult
+            title="No Questions Found"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. our query could be the next big thing others learn from. Get
+          involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
