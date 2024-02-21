3d9043aaeb3c42a4f8056138abb577d0643e0304
TarakaKoda <taraka.koda@gmail.com>
2024-02-21 11:36:06 +0530

    Build rightsidebar and updated navbar and leftsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import NavBar from "@/components/shared/navbar/NavBar";
 import LeftSideBar from "@/components/shared/sidebar/LeftSideBar";
+import RightSideBar from "@/components/shared/sidebar/RightSideBar";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSideBar
+        <RightSideBar/>
       </div>
       {/* Toaster */}
     </main>
