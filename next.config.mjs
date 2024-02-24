96ac4bb4bd0ca015c854895b36623a72d5ce372d
TarakaKoda <taraka.koda@gmail.com>
2024-02-24 19:33:55 +0530

    Updated nextjs config file to allow all traffic for the images.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     mdxRs: true,
     serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      { protocol: "https", hostname: "*" },
+    ],
   },
 };
 
