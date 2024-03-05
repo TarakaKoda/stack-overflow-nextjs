a7219da7de1734aeb1f24b62ab2c385e55dd3013
TarakaKoda <taraka.koda@gmail.com>
2024-03-05 22:52:00 +0530

    Build search functionality for the local search bar

--- components/shared/search/LocalSearchBar.tsx
+++ components/shared/search/LocalSearchBar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
+import { formUrlQuery, removeKeysUrlQuery } from "@/lib/utils";
 import Image from "next/image";
-import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface CustomInputProps {
   route: string;
@@ -19,6 +21,37 @@ const LocalSearchBar = ({
   placeholder,
   otherClasses,
 }: CustomInputProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("q");
+
+  const [search, setSearch] = useState(query || "");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "q",
+          value: search,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["q"],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, route, pathname, router, searchParams, query]);
+
   return (
     <div
       className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
@@ -35,9 +68,11 @@ const LocalSearchBar = ({
       <Input
         type="text"
         placeholder={placeholder}
-        value=""
-        className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
-        onChange={() => {}}
+        value={search}
+        className="paragraph-regular no-focus text-dark100_light900 placeholder background-light800_darkgradient border-none shadow-none outline-none"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
       />
       {iconPosition === "right" && (
         <Image
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
+import qs from "query-string";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -62,4 +63,48 @@ export const getTimestamp = (createdAt: Date): string => {
   }
 
   return "Just now";
+};
+
+interface UrlQueryParams {
+  params: string;
+  key: string;
+  value: string | null;
+}
+
+export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+  const currentUrl = qs.parse(params);
+
+  currentUrl[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true },
+  );
+};
+
+interface RemoveKeysUrlQueryParams {
+  params: string;
+  keysToRemove: string[];
+}
+
+export const removeKeysUrlQuery = ({
+  params,
+  keysToRemove,
+}: RemoveKeysUrlQueryParams) => {
+  const currentUrl = qs.parse(params);
+  keysToRemove.forEach(
+    (key) => {
+      delete currentUrl[key];
+    } 
+  );
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true },
+  );
 };
